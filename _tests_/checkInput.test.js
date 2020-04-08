3037151db9f1dff9f91822e5fa573058abce9e45
Orane Ellis <57927175+ellisorane@users.noreply.github.com>
2020-04-08 00:41:38 -0400

    Delete checkInput.test.js

--- _tests_/checkInput.test.js
+++
@@ -1,7 +0,0 @@
-import { checkPlaceInput } from './checkInput';
-
-test("Check that city is is valid", () => {
-    const pass = checkPlaceInput('Tokyo');
-    //const fail = checkPlaceInput('32 Metz Rd'); 
-    expect(pass);
-})
